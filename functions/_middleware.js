b346098fc86c6bd286b2d656e0118c5f12276bf4
Andrew Stankus <astankus28@gmail.com>
2026-04-14 23:26:35 -0400

    updates middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -5,7 +5,13 @@ export async function onRequest(context) {
   // Block junk product URLs: capitalized slug with trailing number ID
   const junkPattern = /\/[A-Z][A-Za-z0-9\-]+-\d+\/$/;
 
-  if (junkPattern.test(path)) {
+  // Block old WordPress tag archive URLs
+  const tagPattern = /^\/tag\//;
+
+  // Block old WebSphere Commerce URLs
+  const webappPattern = /^\/webapp\//;
+
+  if (junkPattern.test(path) || tagPattern.test(path) || webappPattern.test(path)) {
     return new Response("Not Found", {
       status: 404,
       headers: { "Content-Type": "text/plain" }
